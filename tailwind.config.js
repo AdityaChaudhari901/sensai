08f75caa053ff827e199cbeb0d31a94adead0889
Aditya Chaudhari <adityachaudhari901@gmail.com>
2025-03-10 12:53:34 +0530

    gg

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./app/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          background: 'var(--background)',
+          foreground: 'var(--foreground)',
+        },
+        fontFamily: {
+          sans: 'var(--font-sans)',
+          mono: 'var(--font-mono)',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
